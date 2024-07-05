11abe3c79d61697e959c5f25076008036cadc025
techDhananjay <dhananjay372001@gmail.com>
2024-07-05 17:13:54 +0530

    carousal

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,24 @@
 import Div1 from "@/components/Div1";
-import Div10 from "@/components/Div10";
 import Div6 from "@/components/Div6";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import Carousal from "@/components/Carousal";
+import ReviewButton from "@/components/ReviewButton";
+
 
 export default function Home() {
   return (
-   <div>
-     <Navbar/>
-     <Div1/>
-     <Div6/>
-     <Div10/>
-     <Footer/>
-   </div>
+    <div>
+      <Navbar />
+      <Div1 />
+      <Div6 />
+      <main className="grid content-center overflow-hidden gap-4 w-full text-navy bg-white">
+        <Carousal />
+      </main>
+      <div className="bg-white py-12">
+        <ReviewButton />
+      </div>
+      <Footer />
+    </div>
   );
 }
