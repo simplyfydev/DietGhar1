4676ab617ac59227b316cda1d44bb9ff905fc541
Garvit <garvitchopra22@gmail.com>
2024-07-25 21:43:49 +0530

    commit by garvit

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -8,6 +8,7 @@ import img4 from "@/assets/HowWorks3.png";
 import img5 from "@/assets/HowWorks4.png";
 import img6 from "@/assets/HowWorks5.png";
 import img7 from "@/assets/HowWorks6.png";
+import img8 from "@/assets/HowWorks7.png";
 export default function HowItWorks() {
   return (
     <>
@@ -41,7 +42,8 @@ export default function HowItWorks() {
         </div>
         <div className="flex-1 p-12  place-items-end">
           <h2 className="text-3xl font-semibold text-gray-900 mb-4 place-items-end text-right">
-          &#8220;We provide planning for a better lifestyle with the help of good
+            &#8220;We provide planning for a better lifestyle with the help of
+            good
             <span className="text-[#4A6213]"> diet plans</span>&#8221;
           </h2>
           <div className="mt-6 flex justify-end">
@@ -62,8 +64,8 @@ export default function HowItWorks() {
               &#x2022; Share Your Details
             </h2>
             <p className="mt-4 text-lg text-gray-600">
-              Provide us with your age&#44; weight&#44; height&#44; and any health
-              conditions or dietary restrictions.
+              Provide us with your age&#44; weight&#44; height&#44; and any
+              health conditions or dietary restrictions.
             </p>
           </div>
           <div className="hidden w-1/4 md:flex md:w-1/2 justify-center"></div>
@@ -116,8 +118,9 @@ export default function HowItWorks() {
           How DietGhar Works
         </h1>
         <p className="text-black text-center text-lg mb-24 w-2/5 items-center justify-center mx-auto">
-          At DietGhar&#44; we are dedicated to helping you achieve optimal health
-          and wellness through personalized diet plans. Here&apos;s how we do it:
+          At DietGhar&#44; we are dedicated to helping you achieve optimal
+          health and wellness through personalized diet plans. Here&apos;s how
+          we do it:
         </p>
         <div className="flex h-auto w-full justify-center items-center px-24">
           <div className="w-3/4 px-24">
@@ -125,9 +128,9 @@ export default function HowItWorks() {
               &#x2022; Initial Assessment:
             </h2>
             <p className="text-lg text-gray-600">
-              We start by gathering essential information about you&#44; including
-              your age&#44; weight&#44; height&#44; and any existing health conditions or
-              dietary preferences.
+              We start by gathering essential information about you&#44;
+              including your age&#44; weight&#44; height&#44; and any existing
+              health conditions or dietary preferences.
             </p>
           </div>
           <div className="hidden w-1/4 md:flex justify-center">
@@ -143,9 +146,9 @@ export default function HowItWorks() {
               &#x2022; Customized Diet Plan:
             </h2>
             <p className="text-lg text-gray-600">
-              Based on your details&#44; we create a weekly diet chart that outlines
-              every meal with specific nutritional information like calories&#44;
-              fats&#44; carbohydrates&#44; and more.
+              Based on your details&#44; we create a weekly diet chart that
+              outlines every meal with specific nutritional information like
+              calories&#44; fats&#44; carbohydrates&#44; and more.
             </p>
           </div>
         </div>
@@ -155,9 +158,9 @@ export default function HowItWorks() {
               &#x2022; Ease and Convenience:
             </h2>
             <p className="text-lg text-gray-600">
-              With our comprehensive meal plans&#44; you can enjoy your food without
-              the hassle of planning and worrying about nutritional content. We
-              handle all the details for you.
+              With our comprehensive meal plans&#44; you can enjoy your food
+              without the hassle of planning and worrying about nutritional
+              content. We handle all the details for you.
             </p>
           </div>
           <div className="hidden w-1/4 md:flex justify-center">
@@ -174,10 +177,47 @@ export default function HowItWorks() {
             </h2>
             <p className="text-lg text-gray-600">
               Our services go beyond diet planning. We emphasize overall
-              well-being by incorporating yoga practices&#44; healthy recipes&#44; and
-              fitness tips into your plan.
+              well-being by incorporating yoga practices&#44; healthy
+              recipes&#44; and fitness tips into your plan.
             </p>
           </div>
+        </div>
+      </div>
+
+
+      <div className="bg-white p-16">
+        <h1 className="text-black text-center text-2xl">
+          Join DietGhar today and take the first step towards a healthier&#44;
+          happier you!
+        </h1>
+        <div className="mt-6 flex items-center justify-center">
+          <button className="flex bg-[#9AC435] text-white px-6 py-3 rounded-full shadow-lg border border-white  hover:bg-[#9AC435]">
+            Start 7 days free trial
+            <FaRegArrowAltCircleRight className="text-xl ml-2 mt-[2px]" />
+          </button>
+        </div>
+      </div>
+      <div className="bg-[#FBFFF2] py-12 px-16">
+        <h1 className="text-black text-2xl">You should know</h1>
+        <div className="mt-6 flex items-center justify-center">
+          <p>
+          &#8220;Writing down a plan doubles the chance of success! In the book
+            Atomic Habits&#44; author James Clear talks about a study in the British
+            Journal of Health Psychology that showed 91% of people who wrote
+            down their intention on a goal (in their case&#44; exercise) followed
+            through. This was more than double that of people who didn't write
+            it down. DietGhar helps you set your intentions every single week by
+            generating specific meal plans for you to follow!&#8221;
+          </p>
+        </div>
+      </div>
+      <div className="flex bg-[#F1F1F1]">
+        <div className="p-16 w-2/3">
+          <h1 className="text-2xl ">Master Your Nutrition with DietGhar</h1>
+          <p className="mr-36 text-lg mt-12">&#8220;Try DietGhar and experience the magic of personalized&#44; stress-free meal planning for a healthier&#44; happier you!&#8221;</p>
+        </div>
+        <div className="w-1/3">
+          <Image src={img8} className="w-54 h-54"/>
         </div>
       </div>
     </>
